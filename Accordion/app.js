96f9954c8b7e435ed1bb5c0dcb724a9512d029ed
ulvikrml <ulvikrml@gmail.com>
2022-06-23 21:48:47 +0400

    v2

--- Accordion/app.js
+++ Accordion/app.js
@@ -1,19 +1,12 @@
-let accordion=document.querySelectorAll(".row");
+let accordion=document.querySelectorAll('.row');
 
 
 accordion.forEach(data =>{
     data.addEventListener('click',function (){
-        
         let icon = data.children[1];
-        icon.classList.toggle("active"); 
+        icon.classList.toggle('active'); 
 
         let panel = data.children[2];
-              
-        if(panel.style.display==="block"){
-            panel.style.display="none"
-        }        
-        else{
-            panel.style.display="block";
-        }
+        panel.classList.toggle('d-block')
     })
 })
